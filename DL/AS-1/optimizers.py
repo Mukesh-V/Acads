84cde3fa34291a16575ba6a8488f79dbbb8bc5f9
Mukesh-V <mukeshvinayak2001@gmail.com>
2023-03-19 01:19:36 +0530

    optimizers added

--- DL/AS-1/optimizers.py
+++ DL/AS-1/optimizers.py
@@ -1,11 +1,39 @@
 import numpy as np
 
-def gd(Wb, grads, eta, L):
-    for i in range(2*L):
-        Wb[i] = Wb[i] - eta*grads[i]
+def gd(Wb, grads, eta):
+    L = len(Wb[0])
+    for i in range(2):
+        for j in range(L):
+            Wb[i][j] -= eta * grads[i][j]
     return Wb
 
-def momentum(Wb, grads, eta, gamma, history, L):
-    for i in range(2*L):
-        history[i] = gamma*history[i] + eta*grads[i]
-        Wb[i] = Wb[i]
+def momentum(Wb, grads, eta, gamma, history):
+    L = len(Wb[0])
+    for i in range(2):
+        for j in range(L):
+            history[i][j] = gamma * history[i][j] + eta * grads[i][j]
+            Wb[i][j] -= history[i][j]
+    return Wb, history
+
+def rmsprop(Wb, grads, eta, history, beta, e):
+    L = len(Wb[0])
+    for i in range(2):
+        for j in range(L):
+            history[i][j] = beta * history[i][j] + (1-beta) * grads[i][j]**2
+            Wb[i][j] -= (eta/(np.sqrt(history[i][j]) + e)) * grads[i][j]
+    return Wb, history
+
+def adam(Wb, grads, eta, m, v, beta1, beta2, e, t):
+    L = len(Wb[0])
+    m_hat = [[[] for t in range(L)], [[] for t in range(L)]]
+    v_hat = [[[] for t in range(L)], [[] for t in range(L)]]
+    for i in range(2):
+        for j in range(L):
+            m[i][j] = beta1 * m[i][j] + (1-beta1) * grads[i][j]
+            v[i][j] = beta2 * v[i][j] + (1-beta2) * grads[i][j]**2
+
+            m_hat[i][j] = m[i][j] / (1-np.power(beta1, t))
+            v_hat[i][j] = v[i][j] / (1-np.power(beta2, t))
+
+            Wb[i][j] -= (eta/(np.sqrt(v[i][j]) + e)) * m[i][j]
+    return Wb, m, v
