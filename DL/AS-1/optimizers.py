10317839ec4d9470861d6c9bdcc1c47b7e108568
Mukesh-V <mukeshvinayak2001@gmail.com>
2023-03-19 14:59:23 +0530

    added nadam

--- DL/AS-1/optimizers.py
+++ DL/AS-1/optimizers.py
@@ -35,5 +35,23 @@ def adam(Wb, grads, eta, m, v, beta1, beta2, e, t):
             m_hat[i][j] = m[i][j] / (1-np.power(beta1, t))
             v_hat[i][j] = v[i][j] / (1-np.power(beta2, t))
 
-            Wb[i][j] -= (eta/(np.sqrt(v[i][j]) + e)) * m[i][j]
+            Wb[i][j] -= (eta/(np.sqrt(v[i][j]) + e)) * m_hat[i][j]
+    return Wb, m, v
+
+def nadam(Wb, grads, eta, m, v, beta1, beta2, e, t):
+    L = len(Wb[0])
+    m_hat = [[[] for t in range(L)], [[] for t in range(L)]]
+    v_hat = [[[] for t in range(L)], [[] for t in range(L)]]
+    m_bar = [[[] for t in range(L)], [[] for t in range(L)]]
+    for i in range(2):
+        for j in range(L):
+            m[i][j] = beta1 * m[i][j] + (1-beta1) * grads[i][j]
+            v[i][j] = beta2 * v[i][j] + (1-beta2) * grads[i][j]**2
+
+            m_hat[i][j] = m[i][j] / (1-np.power(beta1, t))
+            v_hat[i][j] = v[i][j] / (1-np.power(beta2, t))
+
+            m_bar[i][j] = beta1 * m_hat[i][j] + (1-beta1)*grads[i][j]
+
+            Wb[i][j] -= (eta/(np.sqrt(v[i][j]) + e)) * m_bar[i][j]
     return Wb, m, v
