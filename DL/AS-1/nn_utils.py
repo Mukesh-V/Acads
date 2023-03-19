back to local sys and fixed backprop

DL/AS-1/nn_utils.py
@@ -25,7 +25,7 @@ def nn_init(sizes, mode='dense', imode='random'):
 
     return Wb, grads, history
     
-def forward(W, H, b, activation, mode='linear'):
+def forward(H, W, b, activation, mode='linear'):
     z = None
     if mode == 'linear':
         W = np.asarray(W)
@@ -35,7 +35,7 @@ def forward(W, H, b, activation, mode='linear'):
 
 def forward_propagation(X, Wb, activation):
     H = X
-    Hs, As = [], [H]
+    Hs, As = [H], []
     for i in range(len(Wb[0])):
         Hi = H
         if i == len(Wb[0])-1: activation = 'softmax'
@@ -54,7 +54,7 @@ def backward(z, activation='sigmoid'):
     
 def backpropagation(Wb, y_hat, y, activation, decay, loss, Hs, As):
     L = len(Wb[0])
-    nc, ny = y_hat
+    nc, ny = y_hat.shape
     y_ohe = []
     for i in range(ny):
         y_ohe.append(one_hot(y[i], nc))
@@ -63,16 +63,15 @@ def backpropagation(Wb, y_hat, y, activation, decay, loss, Hs, As):
     if loss == "cross_entropy":
         da = -(y_ohe - y_hat)
     elif loss == "mse":
-        da = (y_hat - y_ohe)*y_hat - y_hat*(np.dot(y_hat - y).T, y_hat)
+        da = (y_hat - y_ohe)*y_hat - y_hat*(np.dot((y_hat - y).T, y_hat))
 
     grads = [[[] for t in range(L)], [[] for t in range(L)]]
     for j in range(L-1, -1, -1):
-        dW = np.matmul(da, np.transpose(Hs[j])) + 2*decay*Wb[j]
+        dW = np.matmul(da, np.transpose(Hs[j])) + 2*decay*Wb[0][j]
         db = da
-        if not j:
-            dh = np.dot(Wb[j].T, da)
+        if j:
+            dh = np.dot(Wb[0][j].T, da)
             da = np.multiply(dh, backward(As[j-1], activation))
         grads[0][j] = dW
         grads[1][j] = db
-    
     return grads